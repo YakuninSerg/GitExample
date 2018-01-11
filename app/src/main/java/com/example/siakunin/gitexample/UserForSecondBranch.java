60f5a8315f768c35d5d410acd9ba7632c6647086
siakunin <79031587670@mail.ru>
2018-01-11 14:09:54 +0300

    Added UserForSecondBranch

---
+++ app/src/main/java/com/example/siakunin/gitexample/UserForSecondBranch.java
@@ -0,0 +1,9 @@
+package com.example.siakunin.gitexample;
+
+/**
+ * Created by siakunin on 11.01.2018.
+ */
+
+public class UserForSecondBranch {
+    public static final String CONSTANT = "CONSTANTA";
+}

d26c845d1e6557f56c0229a2d55bd3ee61645ded
siakunin <79031587670@mail.ru>
2018-01-11 14:04:09 +0300

    Added user class

---
+++ app/src/main/java/com/example/siakunin/gitexample/User.java
@@ -0,0 +1,15 @@
+package com.example.siakunin.gitexample;
+
+/**
+ * Created by siakunin on 11.01.2018.
+ */
+
+public class User {
+    private String name;
+    private int age;
+    private String address;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
